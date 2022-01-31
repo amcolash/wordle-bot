4425eeaa076a5c4a612c0424851b3ba92a523407
Andrew McOlash <amcolash@gmail.com>
2022-01-31 00:07:00 -0800

    send ifttt notification, use secret

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,11 @@
 
 const { readFileSync } = require('fs');
 const { join } = require('path');
+const fetch = require('node-fetch');
 
 const { generateStats, main, tests, setDebug } = require('./bot');
+
+const words = JSON.parse(readFileSync(join(__dirname, '../data/words.json')).toString());
 
 const argv = require('yargs')
   .scriptName('wordle-bot')
@@ -27,12 +30,11 @@ const argv = require('yargs')
 const debug = argv.debug || false;
 const testMode = argv.test || false;
 let word = argv._[0];
+let wordIndex;
 
 if (!word) {
-  const startDate = new Date('June 19, 2021');
-  const wordIndex = Math.floor((new Date() - startDate) / (1000 * 60 * 60 * 24));
-
-  const words = JSON.parse(readFileSync(join(__dirname, '../data/words.json')).toString());
+  const startDate = new Date('June 19, 2021 UTC');
+  wordIndex = Math.floor((new Date() - startDate) / (1000 * 60 * 60 * 24));
   word = words.answers[wordIndex];
 }
 if (argv.random) word = undefined;
@@ -45,4 +47,25 @@ if (testMode) {
 } else {
   const results = main(stats, word);
   console.log(results);
+
+  const key = process.env.IFTTT_TOKEN;
+  if (key) {
+    const event = 'wordle-bot';
+    const notifUrl = `https://maker.ifttt.com/trigger/${event}/with/key/${key}`; // TODO: Env Var
+
+    const notification = `Wordle ${wordIndex || words.answers.indexOf(word) || ''}, ${results.correct ? 'I got it!' : 'Dang, I failed!'} ${
+      results.guesses
+    }/6\n${results.progress.map((p) => p.results).join('\n')}`;
+
+    console.log(`Sending notification:\n${notification}`);
+
+    fetch(notifUrl, {
+      method: 'post',
+      body: JSON.stringify({ value1: notification }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.text())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }
 }
